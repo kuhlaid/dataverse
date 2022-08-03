9e58a01e5b0d4ffd37774cef949500f19b8c5c1b
kuhlaid <41551429+kuhlaid@users.noreply.github.com>
2022-08-03 07:50:07 -0400

    Update 0prep_deps.sh
    Finished testing

--- conf/docker-aio/0prep_deps.sh
+++ conf/docker-aio/0prep_deps.sh
@@ -5,13 +5,12 @@ if [ ! -d dv/deps ]; then
 	mkdir -p dv/deps
 fi
 
-# installing payara using 
-# if [ ! -e dv/deps/payara-5.2021.6.zip ]; then
-# 	echo "payara dependency prep"
-# 	wget https://s3-eu-west-1.amazonaws.com/payara.fish/Payara+Downloads/5.2021.6/payara-5.2021.6.zip  -O dv/deps/payara-5.2021.6.zip
-# fi
+if [ ! -e dv/deps/payara-5.2021.6.zip ]; then
+	echo "payara dependency prep"
+	wget https://s3-eu-west-1.amazonaws.com/payara.fish/Payara+Downloads/5.2021.6/payara-5.2021.6.zip  -O dv/deps/payara-5.2021.6.zip
+fi
 
-# if [ ! -e dv/deps/solr-8.11.1dv.tgz ]; then
-# 	echo "solr dependency prep"	
-# 	wget https://archive.apache.org/dist/lucene/solr/8.11.1/solr-8.11.1.tgz -O dv/deps/solr-8.11.1dv.tgz
-# fi
+if [ ! -e dv/deps/solr-8.11.1dv.tgz ]; then
+	echo "solr dependency prep"	
+	wget https://archive.apache.org/dist/lucene/solr/8.11.1/solr-8.11.1.tgz -O dv/deps/solr-8.11.1dv.tgz
+fi
